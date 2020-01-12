0791aeecdc3d800eec6b84c73ebf62e23b7fc7d8
Krzyhau <krzyht@gmail.com>
2020-01-12 23:41:21 +0100

    cvars locked

--- smsm/src/Cheats.cpp
+++ smsm/src/Cheats.cpp
@@ -9,8 +9,31 @@
 
 #include "DevControl.hpp"
 
+Variable sv_transition_fade_time;
+Variable ui_loadingscreen_transition_time;
+Variable ui_loadingscreen_fadein_time;
+Variable ui_loadingscreen_mintransition_time;
+
 void Cheats::Init()
 {
+    sv_transition_fade_time = Variable("sv_transition_fade_time");
+    ui_loadingscreen_transition_time = Variable("ui_loadingscreen_transition_time");
+    ui_loadingscreen_fadein_time = Variable("ui_loadingscreen_fadein_time");
+    ui_loadingscreen_mintransition_time = Variable("ui_loadingscreen_mintransition_time");
+
+    sv_transition_fade_time.Modify(FCVAR_DEVELOPMENTONLY, FCVAR_CHEAT | FCVAR_HIDDEN);
+    sv_transition_fade_time.SetValue(0);
+    sv_transition_fade_time.ThisPtr()->m_pszDefaultValue = "0";
+    ui_loadingscreen_transition_time.Modify(FCVAR_DEVELOPMENTONLY, FCVAR_CHEAT | FCVAR_HIDDEN);
+    ui_loadingscreen_transition_time.SetValue(0);
+    ui_loadingscreen_transition_time.ThisPtr()->m_pszDefaultValue = "0";
+    ui_loadingscreen_fadein_time.Modify(FCVAR_DEVELOPMENTONLY, FCVAR_CHEAT | FCVAR_HIDDEN);
+    ui_loadingscreen_fadein_time.SetValue(0);
+    ui_loadingscreen_fadein_time.ThisPtr()->m_pszDefaultValue = "0";
+    ui_loadingscreen_mintransition_time.Modify(FCVAR_DEVELOPMENTONLY, FCVAR_CHEAT | FCVAR_HIDDEN);
+    ui_loadingscreen_mintransition_time.SetValue(0);
+    ui_loadingscreen_mintransition_time.ThisPtr()->m_pszDefaultValue = "0";
+
     Variable::RegisterAll();
     Command::RegisterAll();
 }
--- smsm/src/Cheats.hpp
+++ smsm/src/Cheats.hpp
@@ -10,3 +10,8 @@ public:
 
 extern Command sm_mode;
 extern Command sm_param;
+
+extern Variable sv_transition_fade_time;
+extern Variable ui_loadingscreen_transition_time;
+extern Variable ui_loadingscreen_fadein_time;
+extern Variable ui_loadingscreen_mintransition_time;
