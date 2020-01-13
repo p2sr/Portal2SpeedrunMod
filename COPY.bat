f3ca3d08823c386fec0aae15433586bf6b38c7b5
Krzyhau <krzyht@gmail.com>
2020-01-13 22:27:44 +0100

    local git integration

---
+++ COPY.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+SET "name=Portal 2 Speedrun Mod"
+SET "dest=C:\Program Files (x86)\Steam\steamapps\sourcemods\%name%"
+SET "vpk=D:\Gry\Steem\steamapps\common\Portal 2\bin\vpk.exe"
+
+ECHO.
+ECHO ====== Attempting to create directory at %dest%... ======
+MKDIR "%dest%"
+
+ECHO.
+ECHO ====== Copying raw files... ======
+xcopy /E /V /Y /I "cfg" "%dest%/cfg"
+xcopy /E /V /Y /I "maps" "%dest%\maps"
+xcopy /E /V /Y /I "resource" "%dest%\resource"
+xcopy /E /V /Y /I "scripts" "%dest%\scripts"
+xcopy /E /V /Y /I "media" "%dest%\media"
+copy /Y "gameinfo.txt" "%dest%\gameinfo.txt"
+
+ECHO.
+ECHO ====== Packing pak01_dir... ======
+"%vpk%" pak01_dir
+copy /Y "pak01_dir.vpk" "%dest%\pak01_dir.vpk"
+DEL pak01_dir.vpk
+
+ECHO.
+ECHO ====== Copying smsm.dll... ======
+copy /Y "smsm\bin\smsm.dll" "%dest%\smsm.dll"
+
+ECHO Done.
