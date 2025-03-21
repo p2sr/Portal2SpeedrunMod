20355ee7080454ad817dc6dc9f8368744bcbe764
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2025-03-21 19:09:13 +1100

    fix: tolerate already-modified value

--- smsm/src/Modules/MaterialSystem.cpp
+++ smsm/src/Modules/MaterialSystem.cpp
@@ -8,41 +8,47 @@
 
 #include "SMSM.hpp"
 
+#define NEW_ALLOC 0x672000 // 6.45 MB
+
 bool MaterialSystem::Init()
 {
 #ifdef _WIN32
-    auto sigSize = "BE 00 60 22 00";
+    auto sigSize = "56 BE ? ? ? ? E8";
     auto sigTerm = "FF 0D ? ? ? ? 75 ? B9";
     auto sigInit = "83 3D ? ? ? ? 00 0F 85 ? ? ? ? 56";
-    auto offSize = 1;
+    auto offSize = 2;
 #else
-    auto sigSize = "81 C3 00 60 22 00";
+    auto sigSize = "81 C3 ? ? ? ? E9";
     auto sigTerm = "83 2D ? ? ? ? 01 74 ? C3";
     auto sigInit = "A1 ? ? ? ? 85 C0 74 ? 83 C0 01";
     auto offSize = 2;
 #endif
-    auto defaultContextSize = Memory::Scan(this->Name(), sigSize, offSize);
-    if (defaultContextSize) {
-        Memory::UnProtect((void *)defaultContextSize, 4);
-        *(uint32_t *)defaultContextSize = 0x672000;
-
-        // OHGODPLEASEHELP: This is very jank. I'm sorry.
-        using _RenderContextTerm = void(__cdecl *)(void);
-        using _RenderContextInit = void(__cdecl *)(void);
-        auto RenderContextTerm = (_RenderContextTerm)Memory::Scan(this->Name(), sigTerm);
-        auto RenderContextInit = (_RenderContextInit)Memory::Scan(this->Name(), sigInit);
-        if (RenderContextTerm && RenderContextInit) {
+    defaultContextSize = (uint32_t *)Memory::Scan(this->Name(), sigSize, offSize);
+    RenderContextTerm = (_RenderContextTerm)Memory::Scan(this->Name(), sigTerm);
+    RenderContextInit = (_RenderContextInit)Memory::Scan(this->Name(), sigInit);
+    if (defaultContextSize && RenderContextTerm && RenderContextInit) {
+        if (*defaultContextSize != NEW_ALLOC) {
+            Memory::UnProtect((void *)defaultContextSize, 4);
+            origContextSize = *defaultContextSize;
+            *defaultContextSize = NEW_ALLOC;
+    
+            // OHGODPLEASEHELP: This is very jank. I'm sorry.
             RenderContextTerm();
             RenderContextInit();
         }
+        return this->hasLoaded = true;
+    } else {
+        return this->hasLoaded = false;
     }
-    this->hasLoaded = !!defaultContextSize;
-
-    return this->hasLoaded;
 }
 
 void MaterialSystem::Shutdown()
 {
+    if (origContextSize) {
+        *defaultContextSize = origContextSize;
+        RenderContextTerm();
+        RenderContextInit();
+    }
 }
 
 MaterialSystem* materialSystem;
--- smsm/src/Modules/MaterialSystem.hpp
+++ smsm/src/Modules/MaterialSystem.hpp
@@ -9,6 +9,14 @@ public:
     bool Init() override;
     void Shutdown() override;
     const char* Name() override { return MODULE("materialsystem"); }
+private:
+    uint32_t *defaultContextSize = nullptr;
+    uint32_t origContextSize = 0;
+
+    using _RenderContextTerm = void(__cdecl *)(void);
+    using _RenderContextInit = void(__cdecl *)(void);
+    _RenderContextTerm RenderContextTerm = nullptr;
+    _RenderContextInit RenderContextInit = nullptr;
 };
 
 extern MaterialSystem* materialSystem;
